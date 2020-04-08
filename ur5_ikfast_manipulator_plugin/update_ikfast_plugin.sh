69cbd2ca8ec94d11509731b96590fdb0fd0a9cfd
lochinasc1017 <792258993@qq.com>
2020-04-08 14:03:09 +0800

    IkFast motion planning plugin

---
+++ ur5_ikfast_manipulator_plugin/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py ur5 manipulator ur5_ikfast_manipulator_plugin /home/shanchuan/catkin_sc/src/ur5_ikfast_manipulator_plugin/src/ur5_manipulator_ikfast_solver.cpp
